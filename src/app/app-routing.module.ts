ad8dfa0fad872af75ef9b9b508aa82fb0ec11630
Yogesh <yogeshbietjhs@gmail.com>
2022-09-08 10:51:08 +0530

    Registration modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,4 +13,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [RegistrationComponent,LoginComponent]
+// export const routingComponents = [RegistrationComponent,LoginComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule,routingComponents } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox'
@@ -9,16 +9,16 @@ import {MatCheckboxModule} from '@angular/material/checkbox'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { RegistrationComponent } from './registration/registration.component';
-// import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    // routingComponents
 
-    // RegistrationComponent,
-    // LoginComponent
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
