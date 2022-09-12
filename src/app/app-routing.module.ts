55d49dab5e04a5fb4e74303eb115a65c0e57982a
Yogesh <yogeshbietjhs@gmail.com>
2022-09-12 11:08:12 +0530

    password reset token send and Reset link opened

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { EmailComponent } from './components/email/email.component';
+// given emailcomponents password and confirm password for reset link
 import { ForgetPasswordComponent } from './components/forget-password/forget-password.component';
 
 const routes: Routes = [
   {path:'registration', component:RegistrationComponent},
   {path:'login', component:LoginComponent},
-  {path:'forgetemail', component:EmailComponent},
-  {path:'forgetpassword', component:ForgetPasswordComponent}
+  {path:'forgetemail', component:ForgetPasswordComponent},
+  {path:'User/ResetPassword/:token', component:EmailComponent}
 ];
 
 @NgModule({
