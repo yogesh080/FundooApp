c64b2d96fbc7206db84d22a98ecc7a0878bc17a7
Yogesh <yogeshbietjhs@gmail.com>
2022-09-20 17:59:29 +0530

    Update done in Dialog box

--- src/app/components/trash/trash.component.ts
+++ src/app/components/trash/trash.component.ts
@@ -8,19 +8,21 @@ import { NoteServicesService } from 'src/app/services/noteService/note-services.
 })
 export class TrashComponent implements OnInit {
 
-  trashNotes :[];
+  trashNotes :any;
+
   @Input() NotesArray:any;
 
   constructor(private note: NoteServicesService) { }
 
   ngOnInit(): void {
+    this.getAllNoteTrash();
   }
 
   getAllNoteTrash(){
     console.log("Note for trash")
-    this.note.getallNote().subscribe((response: any) => {
-      console.log("request data", response)
-      this.trashNotes = response.data
+    this.note.getallNote().subscribe((request: any) => {
+      console.log("request data", request)
+      this.trashNotes = request.data
 
       console.log(this.trashNotes)
     })
